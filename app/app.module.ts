7e4f2b81dd4f6db6d07b8837be88e53f6f75d5ce
ZANICOL <nicolaszakgh@gmail.com>
2019-09-11 19:00:56 +0300

    2 files modified, 4 added

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { DvdListComponent } from './dvd-list/dvd-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, DvdListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
