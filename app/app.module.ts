9aec126b3072518228b60749ee83928d50543645
ZANICOL <nicolaszakgh@gmail.com>
2019-09-19 00:00:39 +0300

    4 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DvdListComponent } from './dvd-list/dvd-list.component';
 import { PmStarComponent } from './pm-star/pm-star.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule  ],
   declarations: [ AppComponent, HelloComponent, DvdListComponent, PmStarComponent ],
   bootstrap:    [ AppComponent ]
 })
